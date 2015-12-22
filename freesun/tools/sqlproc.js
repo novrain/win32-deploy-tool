2ab2ff05cd0ca86d9f98d4eda68b2d1ecdc946a9
wangliang <wang.liang@freesun.com.cn>
2015-12-22 20:49:48 +0800

    (*) 修改日志记录的错误

--- freesun/tools/sqlproc.js
+++ freesun/tools/sqlproc.js
@@ -9,24 +9,24 @@ module.exports = function (command, onData) {
     var sqlcmd = process.spawn(command.path, command.args),
         log = function (message) {
             message = message.toString('utf8');
-            logger.log(message);
+            logger.log('info', message);
             return message;
         },
         stdout = '',
         stderr = '',
         deferred = Q.defer();
     sqlcmd.stdout.on('data', function (message) {
-        var msg = log(message);
+        var msg = log('info', message);
         stdout += msg;
         if (onData) {
             onData(msg);
         }
     });
     sqlcmd.stderr.on('data', function (message) {
-        stderr += log(message);
+        stderr += log('error', message);
     });
     sqlcmd.on('exit', function (code) {
-        logger.log('Exit code: ' + code);
+        logger.log('error', 'Exit code: ' + code);
         if (code > 0) {
             var message = 'sqlcmd failed' + (stderr ? ': \r\n\r\n' + stderr : '.');
             deferred.reject(new Error(message));
